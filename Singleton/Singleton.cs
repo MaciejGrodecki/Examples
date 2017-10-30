3cb61b0e7b47b647c9053a39c5df5b999e289fa4
MaciejGrodecki <maciekg1989@gmail.com>
2017-10-30 08:02:51 +0100

    Add static and lazy singleton implementation

---
+++ Singleton/Singleton.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Singleton
+{
+    public class StaticSingleton
+    {
+        private static StaticSingleton instance;
+
+        private StaticSingleton()
+        {
+
+        }
+
+        static StaticSingleton()
+        {
+            instance = new StaticSingleton();
+        }
+
+        public static StaticSingleton Instance
+        {
+            get { return instance; }
+        }
+    }
+
+    public class LazySingleton
+    {
+        private static readonly Lazy<LazySingleton> instance = new Lazy<LazySingleton>(
+            () => new LazySingleton());
+        
+        public static LazySingleton Instance { 
+            get { return instance.Value; }
+        }
+
+        private LazySingleton()
+        {
+
+        }
+    }
+}
